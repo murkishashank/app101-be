dca5b1a3d96beac59a93578f80d021b077e47506
srinivasbanavath <srinivasnaik77022@gmail.com>
2022-07-22 17:00:10 +0530

    first commit

---
+++ src/main/java/com/example/crud1/UsersController.java
@@ -0,0 +1,50 @@
+package com.example.crud1;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UsersController {
+
+    @Autowired
+    public UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @GetMapping("/AllUsers")
+    public List<UsersEntity> allUsers() {
+        return usersService.getAllUsers();
+    }
+
+    @CrossOrigin
+    @GetMapping("/users/{userId}")
+    public UsersEntity getUsers(@PathVariable("userId") Integer userId) {
+        return usersService.getUserById(userId);
+    }
+
+    @CrossOrigin
+    @PostMapping("/users")
+    public ResponseEntity<UsersEntity> saveEmployeeDetails(@RequestBody UsersEntity record) {
+        usersService.saveUsersDetails(record);
+        return new ResponseEntity<UsersEntity>(record, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delEmployee/{employeeId}")
+    public void deleteUsers(@PathVariable("employeeId") int employeeId) {
+        usersService.delete(employeeId);
+    }
+}
---
+++ src/main/java/com/example/crud1/UsersEntity.java
@@ -0,0 +1,88 @@
+package com.example.crud1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "mbl_num")
+    private String mobileNumber;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "status_flag")
+    private int statusFlag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getStatusFlag() {
+        return statusFlag;
+    }
+
+    public void setStatusFlag(int statusFlag) {
+        this.statusFlag = statusFlag;
+    }
+
+    
+}
