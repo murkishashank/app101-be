faccb545ebfb786edf6173d76c8a58cf90172597
Shashank.M <sunny286.ss@gmail.com>
2022-08-01 18:08:34 +0530

    added userName

--- src/main/java/com/example/crud1/workStatus/WorkStatusEntity.java
+++ src/main/java/com/example/crud1/workStatus/WorkStatusEntity.java
@@ -7,7 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.example.crud1.users.UsersEntity;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -43,6 +48,11 @@ public class WorkStatusEntity {
 
     @Column(name = "completed_timestamp")
     public LocalDateTime completedTimeStamp;
+
+    @JsonIncludeProperties({ "userName" })
+    @OneToOne
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private UsersEntity userName;
 
     public int getStatusId() {
         return statusId;
@@ -106,6 +116,14 @@ public class WorkStatusEntity {
 
     public void setCompletedTimeStamp(LocalDateTime completedTimeStamp) {
         this.completedTimeStamp = completedTimeStamp;
+    }
+
+    public UsersEntity getUserName() {
+        return userName;
+    }
+
+    public void setUserName(UsersEntity userName) {
+        this.userName = userName;
     }
 
 }
